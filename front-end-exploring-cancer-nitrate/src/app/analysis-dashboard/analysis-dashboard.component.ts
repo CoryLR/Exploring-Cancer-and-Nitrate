557e89ab605691057ee7cce100b5e725ee51d1eb
UIDCLR <crahman@wisc.edu>
2020-03-01 09:52:18 -0500

    Added legend, touched up UI

--- front-end-exploring-cancer-nitrate/src/app/analysis-dashboard/analysis-dashboard.component.ts
+++ front-end-exploring-cancer-nitrate/src/app/analysis-dashboard/analysis-dashboard.component.ts
@@ -13,7 +13,7 @@ export class AnalysisDashboardComponent implements OnInit {
   private map: any;
   private infoBoxControl;
   private residualsGeoJson: any;
-  private regressionResultsString: string = "";
+  private regressionResultsString: string = "Run the analysis to see regression results.";
   private analysisCount: number = 0;
   private waitingForResults: boolean = false;
 
@@ -26,11 +26,11 @@ export class AnalysisDashboardComponent implements OnInit {
   ngOnInit() {
     this.initializeMap()
   }
-  
+
   runAnalysis = () => {
     console.log("this.idw_power\n", this.idw_power)
     console.log("this.idw_smoothing\n", this.idw_smoothing)
-    if ( !Number.isNaN(this.idw_power) && !Number.isNaN(this.idw_smoothing) && this.idw_power > 0 && this.idw_smoothing >= 0 ) {
+    if (!Number.isNaN(this.idw_power) && !Number.isNaN(this.idw_smoothing) && this.idw_power > 0 && this.idw_smoothing >= 0) {
       this.startAnalysis_updateData(this.idw_power, this.idw_smoothing)
     } else {
       alert("Power must be greater than 0 and Smoothing must be greater than or equal to 0");
@@ -55,33 +55,60 @@ export class AnalysisDashboardComponent implements OnInit {
     this.infoBoxControl = L.control();
 
     this.infoBoxControl.onAdd = function (map) {
-        this._div = L.DomUtil.create('div', 'info_box_control'); // create a div with a class "info"
-        this.update();
-        return this._div;
+      this._div = L.DomUtil.create('div', 'info_control info_control_hover'); // create a div with a class "info"
+      this.update();
+      return this._div;
     };
-    
+
     // method that we will use to update the control based on feature properties passed
     this.infoBoxControl.update = function (props) {
-        this._div.innerHTML = '<h5>Census Tracts</h5>' +  (props ?
+      let residual, cancerRate, nitrateConcentration;
+      if (props) {
+        residual = Number.parseFloat(props.residual).toPrecision(3);
+        cancerRate = Number.parseFloat(props.canrate).toPrecision(2);
+        nitrateConcentration = Number.parseFloat(props.mean).toPrecision(3);
+      }
+      this._div.innerHTML = '<h5>Census Tracts</h5>' + (props ?
+        `
+            <strong>Residual</strong>: ${residual}
+            <br>Cancer Rate: ${cancerRate}
+            <br>Nitrate Concentration: ${nitrateConcentration}
             `
-            Residual: ${Number.parseFloat(props.residual).toPrecision(4)}
-            <br>Cancer Rate: ${Number.parseFloat(props.canrate).toPrecision(4)}
-            <br>Nitrate Concentration: ${Number.parseFloat(props.mean).toPrecision(4)}
-            `
-            : 'Hover over a census tract');
+        : 'Hover over a census tract');
     };
-    
-    this.infoBoxControl.addTo(this.map);
-    
 
-    // /* Useful for testing map position */
-    // setInterval(() => {
-    //   console.log("map center: ", this.map.getCenter());
-    // }, 3000)
+    this.infoBoxControl.addTo(this.map);
+
+    let legend = L.control({ position: 'bottomleft' });
+
+    legend.onAdd = (map) => {
+
+      var div = L.DomUtil.create('div', 'info_control info_control_legend'),
+        // grades = [0, 10, 20, 50, 100, 200, 500, 1000],
+        midGrades = [0.3, 0.2, 0.1, 0, -0.1, -0.2, -0.3],
+        labels = [];
+      
+      div.innerHTML += `<i style="background: ${this.getResidualColor(0.4)}"></i> > 0.3<br>`
+      // loop through our density intervals and generate a label with a colored square for each interval
+      for (var i = 0; i < midGrades.length -1; i++) {
+        div.innerHTML += `<i style="background: ${this.getResidualColor(midGrades[i] - 0.01)}"></i> ${midGrades[i]} - ${midGrades[i+1]}<br>`
+      }
+      div.innerHTML += `<i style="background: ${this.getResidualColor(-0.4)}"></i> < -0.3<br>`
+
+      return div;
+    };
+
+    legend.addTo(this.map);
+
+
+    /* Useful for testing map position */
+    setInterval(() => {
+      console.log("map center: ", this.map.getCenter());
+    }, 5000)
 
   }
 
-  startAnalysis_updateData = (distanceDecayExponent:any = "2", smoothing:any = "0") => {
+  startAnalysis_updateData = (distanceDecayExponent: any = "2", smoothing: any = "0") => {
     this.waitingForResults = true;
     const url = '/analyze';
     const body = { 'distanceDecayExponent': String(distanceDecayExponent), 'smoothing': String(smoothing) };
@@ -90,7 +117,7 @@ export class AnalysisDashboardComponent implements OnInit {
         this.map.removeLayer(this.residualsGeoJson);
         this.residualsGeoJson = undefined;
       }
-      this.regressionResultsString = data.summary;
+      this.regressionResultsString = data.summary.slice(0, -103);
       this.residualsGeoJson = L.geoJSON(JSON.parse(data.geojson), {
         style: this.getStyle("residual"),
         smoothFactor: 0.3,
@@ -102,7 +129,7 @@ export class AnalysisDashboardComponent implements OnInit {
       console.log("this.residualsGeoJson:\n", this.residualsGeoJson)
     });
   }
-  
+
   getStyle = (property) => {
     return (feature) => {
       return {
@@ -115,45 +142,45 @@ export class AnalysisDashboardComponent implements OnInit {
   }
 
   getResidualColor = (d) => {
-    return d < -100 ? 'black' 
-    : d < -0.30 ? '#8c510a' 
-    : d < -0.20 ? '#bf812d' 
-    : d < -0.10 ? '#dfc27d' 
-    : d < -0.00 ? '#f6e8c3' 
-    : d < 0.10 ? '#c7eae5' 
-    : d < 0.20 ? '#80cdc1' 
-    : d < 0.30 ? '#35978f' 
-    : '#01665e';
+    return d < -100 ? 'black'
+      : d < -0.30 ? '#8c510a'
+        : d < -0.20 ? '#bf812d'
+          : d < -0.10 ? '#dfc27d'
+            : d < 0.00 ? '#f6e8c3'
+              : d < 0.10 ? '#c7eae5'
+                : d < 0.20 ? '#80cdc1'
+                  : d < 0.30 ? '#35978f'
+                    : '#01665e';
   }
 
   highlightFeature = (e) => {
     var layer = e.target;
 
     layer.setStyle({
-        weight: 5,
-        color: 'white',
+      weight: 5,
+      color: 'white',
     });
 
     if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
-        layer.bringToFront();
+      layer.bringToFront();
     }
 
     this.infoBoxControl.update(layer.feature.properties);
 
-}
+  }
 
-resetHighlight = (e) => {
-  this.residualsGeoJson.resetStyle(e.target);
-  this.infoBoxControl.update();
-}
+  resetHighlight = (e) => {
+    this.residualsGeoJson.resetStyle(e.target);
+    this.infoBoxControl.update();
+  }
 
-onEachFeature = (feature, layer) => {
-  layer.on({
+  onEachFeature = (feature, layer) => {
+    layer.on({
       mouseover: this.highlightFeature,
       mouseout: this.resetHighlight,
       // click: zoomToFeature
-  });
-}
+    });
+  }
 
 
 }
